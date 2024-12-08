1981f63dfe3d2b0336c374851f46ec3710393d93
luiz2025 <163424597+luiz2025@users.noreply.github.com>
2024-12-07 21:49:07 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+// Função para exibir a mensagem quando o envelope for clicado
+function abrirMensagem() {
+    document.getElementById("mensagemContainer").style.display = "block";
+    document.getElementById("envelope").style.display = "none"; // Esconde o envelope após o clique
+}
